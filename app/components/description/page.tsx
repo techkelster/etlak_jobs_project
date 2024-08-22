e4aeb946fd90ddc269aa824e24ecfa3babd40b3d
techkelster <techkelster@gmail.com>
2024-08-22 07:32:23 +0300

    suspense boundary added

--- app/components/description/page.tsx
+++ app/components/description/page.tsx
@@ -1,33 +1,41 @@
 "use client";
 import MainDescription from "./MainDescription";
 import SideBarDescription from "./SideBarDescription";
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <JobDescriptionPage />
+    </Suspense>
+  );
+}
+
+function JobDescriptionPage() {
   const search = useSearchParams();
   const index = search.get("index");
-  const job = JSON.parse(search.get("job") || "");
+  const job = JSON.parse(search.get("job") || "{}");
+
   return (
-    <>
-      <div className="flex gap-20">
-        <MainDescription
-          description={job.description}
-          responsiblities={job.responsibilities}
-          traits={job.idealCandidate}
-          age={"any"}
-          gender={"any"}
-          whenAndWhere={job.whenAndWhere}
-        />
-        <SideBarDescription
-          posted_on={job.datePosted}
-          deadline={job.deadline}
-          location={job.location.join("")}
-          start_date={job.startDate}
-          end_date={job.endDate}
-          catagories={job.categories}
-          required_skills={job.requiredSkills}
-        />
-      </div>
-    </>
+    <div className="flex gap-20">
+      <MainDescription
+        description={job.description}
+        responsiblities={job.responsibilities}
+        traits={job.idealCandidate}
+        age={"any"}
+        gender={"any"}
+        whenAndWhere={job.whenAndWhere}
+      />
+      <SideBarDescription
+        posted_on={job.datePosted}
+        deadline={job.deadline}
+        location={job.location.join("")}
+        start_date={job.startDate}
+        end_date={job.endDate}
+        catagories={job.categories}
+        required_skills={job.requiredSkills}
+      />
+    </div>
   );
 }
